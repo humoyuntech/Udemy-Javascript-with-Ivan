aaefe0d880ba382e27c5b177b6444c5ce558b37d
Khumoyun <humoyuntech@gmail.com>
2023-08-23 01:31:32 +0500

    Loops: while, do while and for

--- 004.lesson/app.js
+++ 004.lesson/app.js
@@ -17,7 +17,7 @@ let num = 50;
 
 // =========== 3 - The “for” loop ===========
 
-// for(i = 1; i <= 20; i++) {
+// for(let i = 1; i <= 20; i++) {
 //     if(i===6){
         // break; //to'xtadi
        // continue; // tashlab ketadi
