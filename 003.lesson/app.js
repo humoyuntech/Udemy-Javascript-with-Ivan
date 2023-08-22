7bc6ba9b064be4c7ac2bbd9fabf7b2488baf00c7
Khumoyun <humoyuntech@gmail.com>
2023-08-22 13:08:18 +0500

    for loop, ternary operator, switch case statement

---
+++ 003.lesson/app.js
@@ -0,0 +1,40 @@
+"use strict";
+
+//=============== The “for” loop
+let num = 100;
+
+if (num > 100) {
+    console.log("its > 100");  
+} else if(num < 9) {
+    console.log("its < 9");
+} else {
+    console.log("its ok");
+}
+
+//=================== ternary operator
+
+(num ===50) ? console.log("Ok") : console.log("Error");
+
+//==================The switch case statement
+
+let numberOne = 50 + 72;
+console.log(numberOne);
+
+switch(numberOne) {
+    case 50:
+        console.log("its: 50");
+        break;
+    case 122:
+        console.log("its: 122");    
+        break;
+    case 132:
+        console.log("its: 132");    
+        break;  
+    case 150:
+        console.log("its: 150");    
+        break;
+    default: 
+        console.log("I'm sorry");  
+        break;        
+}
+
