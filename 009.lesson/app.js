23b8fc8fb6acf9fa189a88cda2cb327f20e1d985
Khumoyun <humoyuntech@gmail.com>
2023-09-07 16:51:02 +0500

    trim() method

---
+++ 009.lesson/app.js
@@ -0,0 +1,16 @@
+/*
+let mySoft = prompt("sad", "").trim();
+let mySoft2 = prompt("sad", "").trim();
+
+let obj = {};
+obj[mySoft] = mySoft2;
+
+
+console.log(obj);
+*/
+
+    
+let textarrey = " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga aliquid ipsam, accusantium reprehenderit molestias delectus sit? Commodi vel reiciendis expedita laudantium, nobis nemo ad sit! Incidunt excepturi quam fugiat repudiandae? ";
+
+
+console.log(textarrey.trim());
