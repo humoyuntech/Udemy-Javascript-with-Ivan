c3a5b1f490f8b9d18b738ba4983d0c7748d5cf15
Khumoyun <humoyuntech@gmail.com>
2023-09-24 16:38:30 +0500

    Array

---
+++ 12.lesson/app.js
@@ -0,0 +1,52 @@
+"use strict";
+let arr = [1, 2, 3, 4, 5, 6, 8, 10];
+//arrayni oxiridan 1-elementni o'chiradi
+// arr.pop();
+
+//arrayni oxiridan 1-element qo'shadi
+// arr.push(11);
+// console.log(arr);
+//============== Stringni Array qilish
+// let text = "How are you doing today?";
+// const myArray = text.split("");
+// const myArray1 = text.split(" ");
+// const myArray2 = text.split(" ");
+// const myArray3 = text.split(" ", 2);
+
+
+// console.log(myArray);
+// console.log(myArray1);
+// console.log(myArray2);
+// console.log(myArray3);
+// ============================================
+//Arrayni Stringga o'tqazadi
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let text = fruits.join(" ");
+// console.log(text);
+//============================================Sort()
+// arr.sort(comareNum);
+// console.log(arr);
+
+// function comareNum(a, b) {
+//     return a - b;
+// }
+
+// ================
+// for(let i of arr){
+//     console.log(i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
