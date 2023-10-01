f0f258ad84258f030db113c5f5ac765c7118681d
Khumoyun <humoyuntech@gmail.com>
2023-10-01 21:23:03 +0500

    Debugging

---
+++ 017.lesson/app.js
@@ -0,0 +1,23 @@
+"use strict";
+// let num = 5;debugger
+
+// function say() {
+//     let num = 4;debugger
+//     console.log(num);
+// }
+
+// num = 6;debugger
+
+// say();debugger
+
+
+
+
+
+
+
+
+
+
+
+
