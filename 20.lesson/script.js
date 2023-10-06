2068a32de57c05ffd3bdc50354eaf006f6a5b4cf
Khumoyun <humoyuntech@gmail.com>
2023-10-06 22:03:56 +0500

    events and their js handlers

--- 20.lesson/script.js
+++ 20.lesson/script.js
@@ -1,5 +1,7 @@
 const btn = document.querySelector('button');
-
+const btns = document.querySelectorAll('button');
+const overlay = document.querySelector(".overlay");
+const link =document.querySelector('a');
 // ================= eski usul =================
 
 // btn.onclick = function() {
@@ -8,9 +10,9 @@ const btn = document.querySelector('button');
 
 // ================= yangi usul =================
 
-btn.addEventListener('click', () => {
-    alert('Salom dunyo');
-});
+// btn.addEventListener('click', () => {
+//     alert('Salom dunyo');
+// });
 
 // ================= mouseenter-sichqoncha element ustiga kelganda ishga tushadi =================
 
@@ -18,14 +20,24 @@ btn.addEventListener('click', () => {
 //     console.log('Hover');
 // });
 
-// ===========================================
-// btn.addEventListener('mouseenter', (e) => {
-//        console.log(e);
-   // console.log(e.target); //elementga ulanish;
-  //e.target.remove(); //elementni o`chirish;
+// ===================================================================
+
+
+// =========================================== elementga ulanish va elementni o'chirish
+// document.body.addEventListener('click', (e) => {
+   //  console.log(e);
+   //  console.log(e.target); //elementga ulanish;
+  // e.target.remove(); //elementni o'chirish;
+
+  // if (e.target.classList.contains('link'))  {
+  //   alert(123)
+  // }
+
 // });
 
-// bir marta ishlatib hodisani o`chirish
+
+
+// =========================================== bir marta ishlatib hodisani o`chirish
 
 // let i = 0;
 // const deleteElement = (e) => {
@@ -37,11 +49,52 @@ btn.addEventListener('click', () => {
 // };
 
 // btn.addEventListener('click', deleteElement);
+//2-misol
 
-// const link =document.querySelector('a');
+function handleMouseClick(event) {
+  console.log('Вы нажали на элемент:', event.target)
+}
+
+// Добавляем обработчик события
+btn.addEventListener('click', handleMouseClick)
+
+// Убираем обработчик события
+btn.removeEventListener('click', handleMouseClick)
+
+
+//link:(https://doka.guide/js/element-removeeventlistener/)
+
+
+
+
+// ==============================================================
+// ====================всплытие события js=======================
+// ==============================================================
+// const deleteElement = (e) => {
+//     // console.log(e.target);
+//     // console.log(e.currentTarget);
+//     // console.log(e.type);
+// };
+
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+// ===========================================
+
+
 
 // link.addEventListener('click', function(event) {
 //     event.preventDefault();
 
 //     console.log(event.target);
 // });
+
+
+// =================================================
+// =================Bir nechta elemetga o'rnatish===
+// =================================================
+
+// btns.forEach((item) => {
+//   item.addEventListener("click", (e)=> {
+//     console.log(e.target);
+//   })
+// })
