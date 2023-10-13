c2488c4d16ef4461efcb3bc186525e7d1ed29e50
Khumoyun <humoyuntech@gmail.com>
2023-10-13 16:38:38 +0500

    Touch events

---
+++ 024.lesson/app.js
@@ -0,0 +1,47 @@
+"use strict";
+
+//touchstart
+// touchmove
+// touchend
+// touchenter
+// touchleave
+// touchcancel
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+
+    const box = document.querySelector('.box');
+
+    //=== Bosganda ishlaydi
+    // box.addEventListener('touchstart', (e) => {
+    //     e.preventDefault();
+
+    //     console.log('Start');
+    //     console.log(e.targetTouches);
+    // });
+
+
+    //=== Qimirlatganda ishlaydi
+    // box.addEventListener('touchmove', (e) => {
+    //     e.preventDefault();
+
+    //     console.log('Move');
+    // });
+
+    // qo'lni olganda ishlaydi
+    // box.addEventListener('touchend', (e) => {
+    //     e.preventDefault();
+
+    //     console.log('End');
+    // });
+
+});
+
+//touches-qaysi buyruq ishlatilganini ko`rsatadi.
+//targetTouches
+//changedTouches
+
+
+
