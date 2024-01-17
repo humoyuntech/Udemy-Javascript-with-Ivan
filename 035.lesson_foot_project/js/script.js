479484c944cdf61f50e1d11be5e229fd76538194
Khumoyun <humoyuntech@gmail.com>
2024-01-17 13:31:17 +0500

    Food project-4:Creating a modal window

---
+++ 035.lesson_foot_project/js/script.js
@@ -0,0 +1,156 @@
+window.addEventListener("DOMContentLoaded", () => {
+//================> Tabs start
+let tabs = document.querySelectorAll('.tabheader__item'),
+    tabsContent = document.querySelectorAll('.tabcontent'),
+    tabsParent = document.querySelector('.tabheader__items')
+
+/**
+ * 1.keraksiz tablarni yashiramiz:
+ * 2.kerakli tabni ko'rsatamiz:
+ * 3. clickni yaratamiz
+ */
+
+// 1
+function hideTabContent() {
+    tabsContent.forEach(item => {
+        item.classList.add('hide');
+        item.classList.remove('show', 'fade');
+    });
+
+    tabs.forEach(item => {
+        item.classList.remove('tabheader__item_active');
+    });
+};
+
+//2
+function showTabContent(i = 0) {
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+}
+
+hideTabContent();
+showTabContent();
+
+tabsParent.addEventListener('click', (event) =>{
+    const target = event.target;
+    
+    if (target && target.classList.contains('tabheader__item')) {
+        tabs.forEach((item, i) => {
+            if(target == item) {
+                hideTabContent();
+                showTabContent(i);
+            }
+        });
+    }
+});
+
+//================> end tabs
+//================> Timer
+const deadline = '2025-01-1';
+
+function getTimeRemaining(endtime) {
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+
+    if( t <= 0 ) {
+        days = 0;
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+    }else{
+        days = Math.floor(t / (1000 * 60 * 60 * 24));
+        hours = Math.floor((t / (1000 * 60 * 60) % 24));
+        minutes = Math.floor((t / 1000 /60) % 60);
+        seconds = Math.floor((t / 1000) % 60);
+    };
+
+    return {
+        "total": t,
+        "days": days,
+        "hours": hours,
+        "minutes": minutes,
+        "seconds": seconds,
+    };
+};
+
+function getZero(num) {
+    if(num > 0 && num <10) {
+        return `0${num}`;
+    }else{
+        return num;
+    };
+};
+
+function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector('#days');
+    const hours = timer.querySelector('#hours');
+    const minutes= timer.querySelector('#minutes');
+    const seconds = timer.querySelector('#seconds');
+    const timeInterval = setInterval(updateClock, 1000);
+    updateClock();
+
+    function updateClock() {
+        const t = getTimeRemaining(endtime);
+
+        days.innerHTML = getZero(t.days);
+        hours.innerHTML = getZero(t.hours);
+        minutes.innerHTML = getZero(t.minutes);
+        seconds.innerHTML = getZero(t.seconds);
+
+        if(t.total <=0) {
+            clearInterval(timeInterval);
+        };
+    };
+};
+
+setClock('.timer', deadline);
+
+//================> end Timer
+
+//================> Modal
+
+const modalTrigger = document.querySelectorAll('[data-modal]');
+const modal = document.querySelector(".modal");
+const modalCloseBtn = document.querySelector('[data-close]');
+
+modalTrigger.forEach((btn)=> {
+    btn.addEventListener("click", ()=> {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+     
+    });
+});
+
+function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+    
+}
+
+modalCloseBtn.addEventListener("click", closeModal);
+
+modal.addEventListener("click", (e) =>{
+
+    if(e.target === modal) {
+        closeModal();
+    }
+});
+
+document.addEventListener('keydown', (e) =>{
+    if(e.code === "Escape" && modal.classList.contains('show')){
+        closeModal();
+    }
+})
+
+
+
+
+
+
+
+
+});
