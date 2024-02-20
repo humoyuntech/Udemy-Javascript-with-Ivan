f0ceaf0b7a397ff75bea39f6888e71d85d36e791
Khumoyun <humoyuntech@gmail.com>
2024-02-20 14:51:34 +0500

    Food project-16:Build our project using ES6 Modules and fix bugs

---
+++ 062.lesson_foot_project/js/modules/tabs.js
@@ -0,0 +1,55 @@
+function tabs(tabsSelector, tabsContentSelector, tabsParentSelector, activeClass) {
+	let tabs = document.querySelectorAll(tabsSelector),
+		tabsContent = document.querySelectorAll(tabsContentSelector),
+		tabsParent = document.querySelector(tabsParentSelector);
+
+
+    /**
+     * 1.keraksiz tablarni yashiramiz:
+     * 2.kerakli tabni ko'rsatamiz:
+     * 3. clickni yaratamiz
+     */
+
+    // 1
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove(activeClass);
+        });
+    };
+
+    //2
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) =>{
+        const target = event.target;
+        
+        if (target && target.classList.contains(tabsSelector.slice(1))) {
+            tabs.forEach((item, i) => {
+                if(target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+   
+};
+
+export default tabs;
+
+
+
+
