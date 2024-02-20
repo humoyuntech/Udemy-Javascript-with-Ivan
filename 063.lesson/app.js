61fc6696df34ee73a66b6792448a54a9f7ad2afa
Khumoyun <humoyuntech@gmail.com>
2024-02-20 19:45:27 +0500

    try/catch

---
+++ 063.lesson/app.js
@@ -0,0 +1,28 @@
+
+
+
+try {
+    console.log("Hello");
+    console.log(myAbout); //elon qilinmagan o'zgaruvchi
+} catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+    console.log(error.stack);
+}finally {
+    console.log('har qanday holattaham ishlaydi');
+}
+
+console.log("agar xato bo'lsa o'zidan keyingi kodni blocklamaydi");
+
+// 6:34
+
+
+
+
+
+
+
+
+
+
+
