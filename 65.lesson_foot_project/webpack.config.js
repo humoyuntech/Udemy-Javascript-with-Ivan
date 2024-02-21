545c9d487f41e532314a3ea36c9f2c2c707e2aae
Khumoyun <humoyuntech@gmail.com>
2024-02-21 13:23:43 +0500

    Food project-16 The End:How to turn ES6+ code into the old ES5 format. Babel, Core.js and polyfills

---
+++ 65.lesson_foot_project/webpack.config.js
@@ -0,0 +1,35 @@
+'use strict';
+
+let path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './js/script.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/js'
+  },
+  watch: true,
+
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {
+                debug: true,
+                corejs: 3,
+                useBuiltIns: "usage"
+            }]]
+          }
+        }
+      }
+    ]
+  }
+};
+
