a6d125c551edbc55ecebb6749c3c447935a00963
Khumoyun <humoyuntech@gmail.com>
2024-02-29 17:24:42 +0500

    BigInt

---
+++ 070.lesson/app.js
@@ -0,0 +1,14 @@
+console.log("JSdagi Max son:",Number.MAX_SAFE_INTEGER); // JSdagi Max son 9007199254740991
+let num = 9007199254740991115477n;
+console.log(num);
+let someNum = BigInt(9007199254740991115477);
+console.log(someNum);
+
+console.log(5n + 8n);
+console.log(18n / 8n);
+console.log(1n > 8n);
+console.log(1n < 8);
+console.log(8n == 8);
+console.log(8n === 8);
+
+//oddiy sonni bigInt bilan matimatik amal bajarish uchun convertatsiya qilish kerak
